948b787463d35e64208a497c6fb52923b5686480
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-09-07 15:27:09 -0600

    Initial commit

---
+++ src/main/java/com/example/racoonello/RacoonelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.racoonello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacoonelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacoonelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/racoonello/RacoonelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.racoonello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacoonelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
